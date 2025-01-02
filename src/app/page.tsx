b355ee2137db4a5a78caf7f023ee9ea5d2e3fb0b
Martin <mr.mmaurer@gmail.com>
2025-01-02 19:07:08 +0100

    Added pagination

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,60 @@
+import Link from 'next/link';
 import { Suspense } from 'react';
+import { z } from 'zod';
 
 import List from '../components/List/List';
 import getData from '../services/get-data';
 
-export default async function Home() {
-  const getDataPromise = getData(0, 10);
+type HomeProps = {
+  searchParams?: { [key: string]: string | string[] | undefined };
+};
+
+const searchParamsSchema = z.object({
+  offset: z.any().optional().pipe(z.coerce.number()).catch(0),
+  limit: z.any().optional().pipe(z.coerce.number()).catch(10),
+});
+
+export default async function Home({ searchParams }: HomeProps) {
+  const { offset, limit } = searchParamsSchema.parse({
+    offset: (await searchParams)?.offset,
+    limit: (await searchParams)?.limit,
+  });
+
+  const getDataPromise = getData(offset, limit);
+
+  const nextOffset = offset + limit;
+  const prevOffset = Math.max(offset - limit, 0);
 
   return (
-    <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-      <h1>Test</h1>
-      <Suspense fallback={<p>Loading...</p>}>
-        <List itemsPromise={getDataPromise} />
-      </Suspense>
+    <main className="m-4">
+      <h1 className="mb-4">Suspense with use</h1>
+
+      <section>
+        <h2 className="mb-4">Always show suspense fallback when new data is fetched</h2>
+        <Suspense
+          fallback={<p className="motion-safe:animate-pulse">Loading numbers!</p>}
+          key={`${offset}-${limit}`}
+        >
+          <List itemsPromise={getDataPromise} />
+        </Suspense>
+
+        <div className="flex gap-4 mt-4">
+          <Link
+            href={{
+              query: { offset: prevOffset, limit: 10 },
+            }}
+          >
+            Prev
+          </Link>
+          <Link
+            href={{
+              query: { offset: nextOffset, limit: 10 },
+            }}
+          >
+            Next
+          </Link>
+        </div>
+      </section>
     </main>
   );
 }
--- src/services/get-data.ts
+++ src/services/get-data.ts
@@ -1,11 +1,15 @@
 export default async function getData(offset: number, limit: number): Promise<number[]> {
-  const { promise, resolve } = Promise.withResolvers<number[]>();
+  const { promise, resolve, reject } = Promise.withResolvers<number[]>();
+
+  if (offset < 0) {
+    reject(new Error('Offset must be greater than or equal to 0'));
+  }
 
   setTimeout(() => {
     const numbers = Array.from({ length: limit + 1 }, (_, index) => index + offset);
 
     resolve(numbers);
-  }, 2500);
+  }, 3000);
 
   return promise;
 }
